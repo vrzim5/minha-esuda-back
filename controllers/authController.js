c70a68e8866286c20257ed3f41f7707ad4ee0b13
Jullia Azevedo <jullia.azevedo123@gmai.com>
2024-10-31 17:08:27 -0300

    feat: :sparkles: Adiciona autenticação JWT
    Esse commit adiciona autenticação com o Json Web Token

---
+++ controllers/authController.js
@@ -0,0 +1,42 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+exports.registerUser = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "Usuário já existe" });
+    }
+    const user = await User.create({ email, password, name });
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Dados inválidos do usuário" });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "E-mail ou senhas inválidos" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Erro do servidor" });
+  }
+};
