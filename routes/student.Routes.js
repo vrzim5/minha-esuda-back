e671ca8a3cc720807b6024545ba6f1363f341a31
Jullia Azevedo <jullia.azevedo123@gmai.com>
2024-11-04 15:21:45 -0300

    feat: :sparkles: Adiciona as rotas de authentication e student
    Esse commit adiciona os arquivos de rotas authRoutes.js e studentRoutes.js

---
+++ routes/student.Routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  getStudents,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+} = require("../controllers/studentController");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+router.route("/").get(protect, getStudents).post(protect, createStudent);
+router.route("/:id").put(protect, updateStudent).delete(protect, deleteStudent);
+
+module.exports = router;
